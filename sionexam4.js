864a8dc7e189ba855f8bfb6f57718634d18099fb
BanknigPJ <codud3150@gmail.com>
2026-09-03 18:20:28 +0900

    chore: remove accidental git output files

--- sionexam4.js
+++
@@ -1,9 +0,0 @@
-[33m5a8c520[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m)[m HEAD@{0}: reset: moving to 5a8c520847de1c119e6cac57fb0b36fdfcabf599
-[33m1e7d5d8[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m HEAD@{1}: commit: feat.03_js && 99_mission
-[33m5a8c520[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m)[m HEAD@{2}: commit: feat: add common API response and exception handling
-[33m6d1d05b[m HEAD@{3}: commit: chore.JPA DB setting and test
-[33m9f68425[m HEAD@{4}: commit: feat: initialize Spring Boot backend
-[33mf5e7b2d[m HEAD@{5}: commit (merge): chore. resolve README merge conflict
-[33me4b4c5b[m HEAD@{6}: commit: feat. initialize local database infrastructure
-[33mbb5e815[m HEAD@{7}: Branch: renamed refs/heads/main to refs/heads/main
-[33mbb5e815[m HEAD@{9}: commit (initial): feat.1st commit
